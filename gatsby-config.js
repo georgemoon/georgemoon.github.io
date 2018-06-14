0d9e8537a51062b71331cc5ce73d4f08530927ce
George Moon <george@moon.org.nz>
2018-06-14 16:28:35 +1200

    Updates

--- gatsby-config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteMetadata: {
-    title: 'georgemoon.io',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-}
---
+++ path----5db103acd703c6225d03.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{123:function(t,e){t.exports={data:{site:{siteMetadata:{title:"georgemoon.io"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----5db103acd703c6225d03.js.map
